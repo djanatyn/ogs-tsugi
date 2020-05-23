5fa435ad6d9a1ff6e7213e6411e41bf09cc5651e
Jonathan Strickland <djanatyn@gmail.com>
2020-05-23 17:12:54 -0400

    document OGS.Tsugi.Api.Client usage

--- src/OGS/Tsugi/Api/Client.hs
+++ src/OGS/Tsugi/Api/Client.hs
@@ -1,5 +1,40 @@
 {-# LANGUAGE TypeApplications #-}
 
+-- |
+-- Module : OGS.Tsugi.Api.Client
+-- Description : online-go.com client
+--
+-- >>> runOgs $ (getGame ogsClient $ GameId 16945653)
+-- Right
+--   ( Game
+--       { id = 16945653,
+--         name = "VincentL vs. djanatyn",
+--         handicap = 0,
+--         black = 437141,
+--         white = 435842,
+--         rules = "chinese",
+--         gamedata =
+--           GameRecord
+--             { white_player_id = 435842,
+--               black_player_id = 437141,
+--               game_id = 16945653,
+--               komi = 5.5,
+--               rules = "chinese",
+--               winner = 435842,
+--               outcome = "Resignation",
+--               end_time = 1552319896
+--             }
+--       }
+--   )
+--
+-- >>> runOgs $ (getPlayer ogsClient $ PlayerId 435842)
+-- Right
+--   ( Player
+--       { username = "djanatyn",
+--         name = "Jonathan Strickland",
+--         id = 435842
+--       }
+--   )
 module OGS.Tsugi.Api.Client
   ( -- * OGS API Client
     ogsClient,
